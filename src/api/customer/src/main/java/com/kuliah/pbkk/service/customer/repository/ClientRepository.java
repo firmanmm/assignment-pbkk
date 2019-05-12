9ae3769a4d68c7b3bed4d1da909d047f41315765
Rendoru <maulana.firman56@gmail.com>
2019-05-12 13:29:52 +0700

    Add client service

--- src/api/customer/src/main/java/com/kuliah/pbkk/service/customer/repository/ClientRepository.java
+++ src/api/customer/src/main/java/com/kuliah/pbkk/service/customer/repository/ClientRepository.java
@@ -1,9 +1,12 @@
-package com.kuliah.pbkk.service.customer.repository;
+	package com.kuliah.pbkk.service.customer.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.kuliah.pbkk.service.customer.entity.Client;
 
 public interface ClientRepository extends CrudRepository<Client, Long> {
+	@Query("UPDATE FROM clients SET `deleted_at` = CURRENT_TIMESTAMP WHERE id = ?1 AND `deleted_at` IS NULL")
+	public void markDelete(Long id);
 	public Client getByIdentifier(String identifier);
 }
