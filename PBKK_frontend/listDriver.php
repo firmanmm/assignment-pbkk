a928ae1a8d676d4e39a4895cb04a9e277776f6ea
ryoookaze <32873309+ryoookaze@users.noreply.github.com>
2019-05-17 20:41:09 +0700

    Added table for listDriver

--- PBKK_frontend/listDriver.php
+++ PBKK_frontend/listDriver.php
@@ -2,23 +2,21 @@
 
 include_once("config.php");
 
-$result = mysqli_query($mysqli, "SELECT * FROM user ORDER BY id ASC");
+$result = mysqli_query($mysqli, "SELECT * FROM driver ORDER BY id ASC");
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Client List</title>
+    <title>Driver List</title>
 </head>
 <body>
 <table width='80%' border=1>
     <tr>
-        <th>Nama Lengkap</th> <th>Alamat</th> <th>Identitas</th> <th>Email</th> <th>Password</th> <th>Status</th> <th>Update</th>
+        <th>Nama Lengkap</th> <th>Alamat</th> <th>Identitas</th> <th>Email</th> <th>Password</th> <th>Jenis Kendaraan</th> <th>No.Polisi</th> <th>Status</th> <th>Update</>
     </tr>
     <?php
         while($user_data = mysqli_fetch_array($result)) {
@@ -28,10 +26,13 @@ $result = mysqli_query($mysqli, "SELECT * FROM user ORDER BY id ASC");
             echo"<td>".$user_data['noidentitas']."</td>";
             echo"<td>".$user_data['email']."</td>";
             echo"<td>".$user_data['password']."</td>";
-            echo"<td>".$user_data['useractive']."</td>";
+            echo"<td>".$user_data['jeniskendaraan']."</td>";
+            echo"<td>".$user_data['nopolisi']."</td>";
+            echo"<td>".$user_data['status']."</td>";
             echo "<td><a href='edit.php?id=$user_data[id]'>Edit</a> | <a href='delete.php?id=$user_data[id]'>Delete</a></td></tr>";
         }
     ?>
+    
     
 </body>
 </html>
